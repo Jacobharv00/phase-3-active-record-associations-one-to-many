fc8f9d7933a0dbd284c44d2d1e8811c8b4d3956c
jacob <jacobharv00@gmail.com>
2021-07-25 18:12:06 -0600

    Done

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,12 @@
 class Game < ActiveRecord::Base
-  
+  has_many :reviews
 end
+
+
+# If we were to write above by hand it would look like this:
+
+# class Game < ActiveRecord::Base
+#   def reviews
+#     Review.where(game_id: self.id)
+#   end
+# end 
